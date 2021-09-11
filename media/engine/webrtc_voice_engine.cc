1302ecf1f382af604ea789562d05d3bac47c0f0b
CloudWebRTC <duanweiwei1982@gmail.com>
2021-09-11 19:27:32 +0800

    Audio Device Optimization
    allow listen-only mode in AudioUnit, adjust when category changes (#2)
    
    release mic when category changes (#5)
    
    Change defaults to iOS defaults (#7)
    
    Sync audio session config (#8)
    
    feat: support bypass voice processing for iOS. (#15)
    
    Remove MacBookPro audio pan right code (#22)
    
    fix: Fix can't open mic alone when built-in AEC is enabled. (#29)
    
    feat: add audio device changes detect for windows. (#41)
    
    fix Linux compile (#47)
    
    AudioUnit: Don't rely on category switch for mic indicator to turn off (#52)
    
    Stop recording on mute (turn off mic indicator) (#55)
    
    Cherry pick audio selection from m97 release (#35)
    
    [Mac] Allow audio device selection (#21)
    
    RTCAudioDeviceModule.outputDevice / inputDevice getter and setter (#80)
    
    Co-authored-by: Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
    Co-authored-by: David Zhao <dz@livekit.io>

--- media/engine/webrtc_voice_engine.cc
+++ media/engine/webrtc_voice_engine.cc
@@ -1707,6 +1707,9 @@ bool WebRtcVoiceSendChannel::MuteStream(uint32_t ssrc, bool muted) {
     ap->set_output_will_be_muted(all_muted);
   }
 
+  // Notify the AudioState that the mute state has updated.
+  engine_->audio_state()->OnMuteStreamChanged();
+
   return true;
 }
 
