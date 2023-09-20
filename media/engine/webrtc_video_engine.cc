2e753c166fefa5241815f4ead8e2609d580c0e48
Kanat Kiialbaev <kijalbaev@gmail.com>
2023-09-20 12:44:37 -0700

    Android improvements. (#20)
    Start/Stop receiving stream method for VideoTrack (#25)
    
    Properly remove observer upon deconstruction (#26)
    
    feat: Expose setCodecPreferences/getCapabilities for android. (#61)
    
    fix: add WrappedVideoDecoderFactory.java. (#74)
    
    Exposing Adapter types in PeerConnectionFactory (#78)
    
    Co-authored-by: davidliu <davidliu@deviange.net>
    Co-authored-by: Mohamed Risaldar UT <73091075+MohamedRisaldarTA@users.noreply.github.com>
    (cherry picked from commit e91f003820c099422e4d26869fb1eeb1611d72d0)
    
    # Conflicts:
    #	media/base/media_channel.h
    #	media/engine/webrtc_video_engine.cc
    #	media/engine/webrtc_video_engine.h

--- media/engine/webrtc_video_engine.cc
+++ media/engine/webrtc_video_engine.cc
@@ -2604,6 +2604,24 @@ WebRtcVideoReceiveChannel::~WebRtcVideoReceiveChannel() {
     delete kv.second;
 }
 
+void WebRtcVideoReceiveChannel::StartReceive(uint32_t ssrc) {
+  RTC_DCHECK_RUN_ON(&thread_checker_);
+  WebRtcVideoReceiveStream* stream = FindReceiveStream(ssrc);
+  if(!stream) {
+    return;
+  }
+  stream->StartStream();
+}
+
+void WebRtcVideoReceiveChannel::StopReceive(uint32_t ssrc) {
+  RTC_DCHECK_RUN_ON(&thread_checker_);
+  WebRtcVideoReceiveStream* stream = FindReceiveStream(ssrc);
+  if(!stream) {
+    return;
+  }
+  stream->StopStream();
+}
+
 void WebRtcVideoReceiveChannel::SetReceiverFeedbackParameters(
     bool lntf_enabled,
     bool nack_enabled,
@@ -3497,6 +3515,17 @@ void WebRtcVideoReceiveChannel::WebRtcVideoReceiveStream::SetReceiverParameters(
     RecreateReceiveStream();
   } else {
     RTC_DLOG_F(LS_INFO) << "No receive stream recreate needed.";
+  }
+}
+
+void WebRtcVideoReceiveChannel::WebRtcVideoReceiveStream::StartStream(){
+  if (stream_) {
+    stream_->Start();
+  }
+}
+void WebRtcVideoReceiveChannel::WebRtcVideoReceiveStream::StopStream(){
+  if (stream_) {
+    stream_->Stop();
   }
 }
 
