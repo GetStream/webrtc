fef3d8f04caedc84b8c6eb8db67f3f685ae132b7
kanat <kijalbaev@gmail.com>
2023-09-25 16:58:51 -0700

    repackage android/java layer

--- sdk/android/src/java/io/getstream/webrtc/Empty.java
+++ sdk/android/src/java/io/getstream/webrtc/Empty.java
@@ -1,14 +1,17 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!--
- *  Copyright 2017 The WebRTC Project Authors. All rights reserved.
+/*
+ *  Copyright 2018 The WebRTC project authors. All Rights Reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
  *  tree. An additional intellectual property rights grant can be found
  *  in the file PATENTS.  All contributing project authors may
  *  be found in the AUTHORS file in the root of the source tree.
--->
-<manifest xmlns:android="http://schemas.android.com/apk/res/android"
-    package="io.getstream.webrtc">
-  <uses-sdk android:minSdkVersion="21" android:targetSdkVersion="23" />
-</manifest>
+ */
+
+package io.getstream.webrtc;
+
+/**
+ * Empty class for use in libjingle_peerconnection_java because all targets require at least one
+ * Java file.
+ */
+class Empty {}
