5a0da421e3282cf70d1d1a5bc38fdcf7af5b937a
kanat <kijalbaev@gmail.com>
2023-09-26 14:48:06 -0700

    repackage java layer in rtc_base
    (cherry picked from commit 7176e2e9edff9f8a2691aa921148777780268755)

--- rtc_base/java/src/io/getstream/webrtc/Loggable.java
+++ rtc_base/java/src/io/getstream/webrtc/Loggable.java
@@ -8,9 +8,9 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-package org.webrtc;
+package io.getstream.webrtc;
 
-import org.webrtc.Logging.Severity;
+import io.getstream.webrtc.Logging.Severity;
 
 /**
  * Java interface for WebRTC logging. The default implementation uses webrtc.Logging.
