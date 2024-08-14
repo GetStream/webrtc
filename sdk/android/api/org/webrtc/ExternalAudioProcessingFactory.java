d53404a27f1eb6e36382c0e75b671ef2fd9f4806
kanat <kijalbaev@gmail.com>
2024-08-14 16:05:48 -0400

    [PBE-5300] add java external audio wrappers

---
+++ sdk/android/api/org/webrtc/ExternalAudioProcessingFactory.java
@@ -0,0 +1,32 @@
+/*
+ *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+package org.webrtc;
+
+/** Factory for creating webrtc::ExternalAudioProcessing instances. */
+public class ExternalAudioProcessingFactory implements AudioProcessingFactory {
+
+  private ExternalAudioProcessorFactory processorFactory;
+
+  public ExternalAudioProcessingFactory(ExternalAudioProcessorFactory processorFactory) {
+    this.processorFactory = processorFactory;
+  }
+
+  /**
+   * Dynamically allocates a webrtc::ExternalAudioProcessing instance and returns a pointer to it.
+   * The caller takes ownership of the object.
+   */
+  public long createNative() {
+    return nativeCreateExternalProcessing(processorFactory.createNative());
+  }
+
+
+  private static native long nativeCreateExternalProcessing(long processor);
+}
