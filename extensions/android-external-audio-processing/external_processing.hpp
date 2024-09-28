a4a053e4565543704507a4cdb2ebcb2df98b8cfc
Kanat Kiialbaev <kijalbaev@gmail.com>
2024-09-28 12:55:06 -0400

    Patch/android/external audio processing (#37)
    * [PBE-5300] add android-external-audio-processing
    
    * [PBE-5300] add external_processor.cc
    
    * [PBE-5300] clean up jni
    
    * [PBE-5300] clean java layer
    
    * [PBE-5300] rename files & classes
    
    * [PBE-5300] add external processor
    
    * [PBE-5300] fix java compilation
    
    * [PBE-5300] remove NonNull annotation
    
    * [PBE-5300] add missing include
    
    * [PBE-5300] pass external_processor
    
    * [PBE-5300] fix unguarded headers
    
    * [PBE-5300] fix JNI_ExternalAudioProcessingFactory params
    
    * [PBE-5300] change include order
    
    * [PBE-5300] jni experiment 1
    
    * [PBE-5300] jni experiment 2
    
    * [PBE-5300] jni experiment 3
    
    * [PBE-5300] jni experiment 4
    
    * [PBE-5300] jni experiment 5
    
    * [PBE-5300] jni experiment 6
    
    * [PBE-5300] jni experiment 7
    
    * [PBE-5300] jni experiment 8
    
    * [PBE-5300] jni experiment 9
    
    * [PBE-5300] jni experiment 10
    
    * [PBE-5300] jni experiment 11
    
    * [PBE-5300] jni experiment 12
    
    * [PBE-5300] jni experiment 13
    
    * [PBE-5300] jni experiment 14
    
    * [PBE-5300] jni experiment 15
    
    * [PBE-5300] add dynamic processing
    
    * fix nativeGetInstance
    
    * rename to nativeGetApm
    
    * fix compilation
    
    * hardcode name
    
    * fix jni compilation
    
    * fix compilation issue
    
    * fix dynamic_processing.cc
    
    * rename to dynamic_apm_ptr
    
    * convert dynamic to external
    
    * fix compilation issues
    
    * fix nativeDestroyAudioProcessingModule return type
    
    * include external_processor_loader
    
    * update BUILD.gn
    
    * fix loadExternalProcessor return type
    
    * define Create & Destroy
    
    * delete loader include
    
    * fix Load to Create
    
    * migrate to external functions
    
    * clean up logs

---
+++ extensions/android-external-audio-processing/external_processing.hpp
@@ -0,0 +1,49 @@
+#ifndef EXTERNAL_PROCESSING_HPP
+#define EXTERNAL_PROCESSING_HPP
+
+#include <syslog.h>
+
+#include "include/external_processor.hpp"
+
+#include "modules/audio_processing/audio_buffer.h"
+#include "modules/audio_processing/audio_processing_impl.h"
+#include "modules/audio_processing/include/audio_processing.h"
+
+namespace external {
+
+class ExternalProcessing : public webrtc::CustomProcessing {
+ public:
+  ExternalProcessing(const ExternalProcessing&) = delete;
+  ExternalProcessing(ExternalProcessing&&) = delete;
+  ExternalProcessing& operator=(const ExternalProcessing&) = delete;
+  ExternalProcessing& operator=(ExternalProcessing&&) = delete;
+  ~ExternalProcessing();
+
+  static ExternalProcessing* getInstance() {
+    if (m_instance == nullptr) {
+      m_instance = new ExternalProcessing();
+    }
+    return m_instance;
+  }
+
+  bool Create(const char* libname);
+
+  bool Destroy();
+
+  void Initialize(int sample_rate_hz, int num_channels) override;
+  void Process(webrtc::AudioBuffer* audio) override;
+  std::string ToString() const override;
+  void SetRuntimeSetting(
+      webrtc::AudioProcessing::RuntimeSetting setting) override;
+
+ private:
+  ExternalProcessing();
+
+  void* m_handle = nullptr;
+  std::array<void *, kFunctionCount> m_functionPointers = {};
+
+  static ExternalProcessing* m_instance;
+};
+}  // namespace external
+
+#endif  // EXTERNAL_PROCESSING_HPP
