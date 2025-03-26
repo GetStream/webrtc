0538a8d89def2f0a21b9244f7930fe70187bc42e
Harald Alvestrand <hta@webrtc.org>
2025-03-26 20:23:52 +0000

    Deprecate cricket::MediaType and associated aliases
    Also update remaining uses of cricket::MediaType to be webrtc::MediaType
    Removing superfluous prefixes will be done in a later stage as part
    of cleanup after the namespace merge.
    
    Bug: webrtc:42222911
    Change-Id: If5101e2aefe12b6a772ae6af308ee0dca666daba
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/382660
    Commit-Queue: Harald Alvestrand <hta@webrtc.org>
    Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
    Auto-Submit: Harald Alvestrand <hta@webrtc.org>
    Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#44237}

--- media/engine/webrtc_voice_engine.h
+++ media/engine/webrtc_voice_engine.h
@@ -201,7 +201,9 @@ class WebRtcVoiceSendChannel final : public MediaChannelUtil,
 
   ~WebRtcVoiceSendChannel() override;
 
-  MediaType media_type() const override { return webrtc::MediaType::AUDIO; }
+  webrtc::MediaType media_type() const override {
+    return webrtc::MediaType::AUDIO;
+  }
   VideoMediaSendChannelInterface* AsVideoSendChannel() override {
     RTC_CHECK_NOTREACHED();
     return nullptr;
@@ -364,7 +366,9 @@ class WebRtcVoiceReceiveChannel final
 
   ~WebRtcVoiceReceiveChannel() override;
 
-  MediaType media_type() const override { return webrtc::MediaType::AUDIO; }
+  webrtc::MediaType media_type() const override {
+    return webrtc::MediaType::AUDIO;
+  }
 
   VideoMediaReceiveChannelInterface* AsVideoReceiveChannel() override {
     RTC_CHECK_NOTREACHED();
