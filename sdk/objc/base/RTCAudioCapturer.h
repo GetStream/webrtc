9a6a0fab02ae17eb8f37a3d99e23d71f67cfd1c7
Ilias Pavlidakis <ilias.pavlidakis@getstream.io>
2025-09-17 12:54:55 +0300

    1. Implement RTCAudioCapturer

---
+++ sdk/objc/base/RTCAudioCapturer.h
@@ -0,0 +1,37 @@
+/*
+ *  Copyright 2025 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#import <Foundation/Foundation.h>
+
+#import "sdk/objc/base/RTCMacros.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class RTC_OBJC_TYPE(RTCAudioCapturer);
+@class RTC_OBJC_TYPE(RTCAudioBuffer);
+
+RTC_OBJC_EXPORT
+@protocol RTC_OBJC_TYPE
+(RTCAudioCapturerDelegate)<NSObject>
+- (void)capturer:(RTC_OBJC_TYPE(RTCAudioCapturer) *)capturer
+    didCaptureAudioBuffer:(RTC_OBJC_TYPE(RTCAudioBuffer) *)buffer;
+@end
+
+RTC_OBJC_EXPORT
+@interface RTC_OBJC_TYPE (RTCAudioCapturer) : NSObject
+
+@property(nonatomic, weak) id<RTC_OBJC_TYPE(RTCAudioCapturerDelegate)> delegate;
+
+- (instancetype)initWithDelegate:
+    (id<RTC_OBJC_TYPE(RTCAudioCapturerDelegate)>)delegate;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ sdk/objc/base/RTCAudioCapturer.m
@@ -0,0 +1,26 @@
+/*
+ *  Copyright 2025 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#import "RTCAudioCapturer.h"
+
+@implementation RTC_OBJC_TYPE (RTCAudioCapturer)
+
+@synthesize delegate = _delegate;
+
+- (instancetype)initWithDelegate:
+    (id<RTC_OBJC_TYPE(RTCAudioCapturerDelegate)>)delegate {
+  self = [super init];
+  if (self) {
+    _delegate = delegate;
+  }
+  return self;
+}
+
+@end
